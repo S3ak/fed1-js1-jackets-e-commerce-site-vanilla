ca99802b92189690aaa116ea1bb0ba05f2551e78
Monde Sineke <nsineke@gmail.com>
2025-02-05 13:34:24 +0100

    build: :wrench: config mutli-page app for vite

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        productDetails: resolve(__dirname, "product-details.html"),
+      },
+    },
+  },
+});
