620ee7ce4206e8526d946d905ae68ea6e7b990f5
Monde Sineke <nsineke@gmail.com>
2025-02-17 15:15:56 +0100

    fix: show one product details

--- src/products/product-details.mjs
+++ src/products/product-details.mjs
@@ -183,7 +183,7 @@ function handleFormSubmit(event) {
   });
 }
 
-const getIdFromUrl = () => {
+function getIdFromUrl() {
   /**
    * Extracts the 'id' parameter from the URL's query string.
    * @url https://mollify.noroff.dev/content/feu1/javascript-1/module-5/api-advanced/url-parameters?nav=
@@ -202,7 +202,7 @@ const getIdFromUrl = () => {
   const id = searchParameters.get("id");
 
   return id;
-};
+}
 
 function addFormHandlerToDetailsEl(detailsEl = document.createElement()) {
   const addToCartForm = detailsEl.querySelector("form");
